41a50e3e7324df045ac395cce7c628c393d6f703
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-11 14:49:50 +0300

    task 2 promt that outputs even or odd numbers

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 //task 2
 
+var prompt = require("prompt-sync")();
+let taskTwo = parseInt(prompt("Enter a number"));
+const result = taskTwo % 2 === 0 ? "even" : "odd";
+console.log(result);
 //task 3
 
 //task 4
