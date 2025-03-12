87dd13e01ed5be4f12954b76b90ddfec3c2f42bc
Myles <myles.parkire1@student.moringaschool.com>
2025-03-12 09:25:32 +0300

    task1

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//task 1
+//task 1 
+
+// Define the array
+let a = [5, 10, 15, 20, 25];
+
+// Create a new array with the first and last elements
+let newArray = [a[0], a[a.length - 1]];
+
+// Print the new array
+console.log(newArray)
 
 //task 2
 
